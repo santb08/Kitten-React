58c7f64fb7087aac6fd355ca89fd9a1b71125968
fxfxrxfx <santi-c-b@outlook.com>
2019-02-22 00:34:24 -0500

    Updates

---
+++ precache-manifest.651097806a4cd44a4d19d52096ae6682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71d840cb19b5a6aeb331",
+    "url": "/Kitten-React/static/js/runtime~main.71d840cb.js"
+  },
+  {
+    "revision": "82fe91753d23eaa1d0c7",
+    "url": "/Kitten-React/static/js/main.82fe9175.chunk.js"
+  },
+  {
+    "revision": "a7eee8b91dfcb1e99477",
+    "url": "/Kitten-React/static/js/1.a7eee8b9.chunk.js"
+  },
+  {
+    "revision": "82fe91753d23eaa1d0c7",
+    "url": "/Kitten-React/static/css/main.61a5b14d.chunk.css"
+  },
+  {
+    "revision": "72606028ad215a96bb467f66ad3283ef",
+    "url": "/Kitten-React/index.html"
+  }
+];
